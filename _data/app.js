591913d4c940296f013780d603a8a34331146c70
Jonathan MirCha <jonmircha@gmail.com>
2023-04-26 00:33:52 -0600

    Cambios

--- _data/app.js
+++ _data/app.js
@@ -1,7 +1,7 @@
 module.exports = function () {
   let mode = true,
     domainDev = "http://localhost:7777/",
-    domainProd = "https://jonmircha.github.io/lasalle-dgd-2023/",
+    domainProd = "https://lasalle-dgd-2023.vercel.app/",
     domain = mode ? domainProd : domainDev,
     lang = "es",
     themeColor = `#FFF`,
