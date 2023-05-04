f7d16a312120b620f08c15683753238eee624de8
Jonathan MirCha <jonmircha@gmail.com>
2023-05-03 20:13:23 -0600

    Depurando sitio

--- assets/main.js
+++ assets/main.js
@@ -1,7 +1,1 @@
-import { PetCard } from "./components/PetCard.mjs";
-import { MyCounter } from "./components/MyCounter.mjs";
-
-window.customElements.define("pet-card", PetCard);
-window.customElements.define("my-counter", MyCounter);
-
 console.log("Hola Mundo 🦊");
